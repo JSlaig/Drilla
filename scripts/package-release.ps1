8adacda5146dfe797a2fb431cd0aa7c18557231b
JSlaig <jsaligz@gmail.com>
2026-09-08 10:54:38 +0200

    Fix jumphost loop: -J carries only intermediary jumps, last jump is the login host, v0.2.3

--- scripts/package-release.ps1
+++ scripts/package-release.ps1
@@ -12,7 +12,7 @@ if (-not (Test-Path "target\release\drilla.exe")) {
 }
 
 $dist = Join-Path $PSScriptRoot "..\dist"
-$version = "0.2.2"
+$version = "0.2.3"
 $exeName = "drilla-v$version-win64"
 $pkgDir = Join-Path $dist $exeName
 
